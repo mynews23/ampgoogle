4ab58bc3fce899d7feb636e51fbdd99376625019
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-11-10 02:44:13 +0200

    Create contact.tsx

---
+++ pages/contact.tsx
@@ -0,0 +1,44 @@
+import Head from 'next/head'
+import { useAmp } from 'next/amp'
+import Byline from '../components/Byline'
+
+export const config = {
+  amp: 'hybrid',
+}
+
+export default function ContactPage() {
+  const isAmp = useAmp()
+
+  return (
+    <div>
+      <Head>
+      <title>The Maliek News - About Us</title>
+      </Head>
+      <h1>Contact Us</h1>
+      <Byline author="Maliek Galant" />
+      <p>
+        <a href={isAmp ? '/about' : '/about?amp=1'}>
+          {isAmp ? 'View Non-AMP' : 'View AMP'} Mobile Version
+        </a>
+      </p>
+      <p className="caption">Contact</p>
+  
+      <p>
+        Was issued souly by your favorite company
+        {' '}
+        <a href={isAmp ? '/home?amp=1' : '/home'}>Home </a> page. Marzipan gummi
+        bears pie danish lollipop pudding powder gummi bears sweet. Pie sweet
+        roll sweet roll topping chocolate bar dragée pudding chocolate cake.
+        Croissant sweet chocolate bar cheesecake candy canes. Tootsie roll icing
+        macaroon bonbon cupcake apple pie candy canes biscuit candy canes.
+        Jujubes jelly liquorice toffee gingerbread. Candy tootsie roll macaroon
+        chocolate bar icing sugar plum pie. Icing gummies chocolate bar
+        chocolate marzipan bonbon cookie chocolate tart. Caramels danish halvah
+        croissant. Cheesecake cookie tootsie roll ice cream. Powder dessert
+        carrot cake muffin tiramisu lemon drops liquorice topping brownie.
+        Soufflé chocolate cake croissant cupcake jelly.
+      </p>
+      
+    </div>
+  )
+}
